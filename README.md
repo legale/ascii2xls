79d439f3bafa5aaac52632e594f3f14c81e3fe62
rumi <legale.legale@gmail.com>
2020-02-07 12:03:45 +0300

    1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ascii2xls
 python3 script to convert ascii pseudo table to xls or xlsx format
+
+## System requirements
+python3
+python3 modules: 
+bs4 docutils dashtable pandas openpyxl xlwt
+
+## Syntax
+```
+./ascii2xls.py SRC_PSEUDO_TABLE_FILE [DST_CSV_FILE]
+```
+
+## Usage example 
+Run: 
+```
+./ascii2xls text2.txt
+```
+to convert pseudo table in `text2.txt` to xlsx format to `text2.txt.xlsx`
---
+++ ascii2xls.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import sys
+if len(sys.argv) == 1:
+	print ("Usage:", sys.argv[0], "SRC_PSEUDO_TABLE_FILE [DST_CSV_FILE]")
+	exit()
+
+
+from importlib import import_module
+deps = ["bs4", "docutils", "dashtable", "pandas", "openpyxl", "xlwt"]
+not_found = []
+
+for dep in deps:
+	try:
+		import_module(dep)
+	except:
+		not_found.append(dep) 
+
+if len(not_found) > 0:
+	print("Required modules are not installed. Please install: ", ' '.join(not_found))
+	exit()
+
+args_cnt = len(sys.argv)
+if args_cnt == 2:
+	dst = sys.argv[1] + ".xlsx"
+elif args_cnt == 3:
+	dst = sys.argv[2]
+else:
+	print("2 arguments expected at most,", args_cnt, "given.")
+	exit()
+
+import dashtable as dt
+import pandas as pd
+
+try:
+	text = open(sys.argv[1], 'r').read()
+except:
+	print("Unable to read file:", sys.argv[1])
+	exit()
+
+text = text\
+.replace("┌","+").replace("┬","+").replace("┐","+")\
+.replace("├","+").replace("┼","+").replace("┤","+")\
+.replace("└","+").replace("┴","+").replace("┘","+")\
+.replace("│","|").replace("─","-")
+
+	
+table, spans, use_headers = dt.grid2data(text)
+frame = pd.DataFrame(table[1:], columns=table[0])
+frame.to_excel(dst, index=False)
+
+
